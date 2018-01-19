b415890f71d8179e188bd99a4aa37f4a52c1f399
sidneipaixao <34635384+sidneipaixao@users.noreply.github.com>
2018-01-19 02:39:05 +0000

    Correcao

--- Sistema/mdlControle.vb
+++ Sistema/mdlControle.vb
@@ -1,37 +1,37 @@
 ﻿Module mdlControle
 
-  Public Structure tCaixa
-    Dim Situacao As Boolean
-    Dim Horario As Date
-    Dim Usuario As Integer
-  End Structure
+    Public Structure tCaixa
+        Dim Situacao As Boolean
+        Dim Horario As Date
+        Dim Usuario As Integer
+    End Structure
 
-  Public rCaixa As tCaixa
+    Public rCaixa As tCaixa
 
-  Public Function fnCaixaAberto() As Boolean
+    Public Function fnCaixaAberto() As Boolean
 
-    Dim lAguardar As Boolean
+        Dim lAguardar As Boolean
 
-    If Not rCaixa.Situacao Then
-      If MessageBox.Show("A abertura de turno ainda não foi realizada." & vbNewLine & "Deseja efetuar a abertura agora?", "Turno não iniciado", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = DialogResult.Yes Then
-        With frmAbrirTurno
-          '.MdiParent = frmPrincipal
-          .Show()
-          lAguardar = True
-          While lAguardar
-            lAguardar = False
-            For Each oFormulario In Application.OpenForms
-              If TypeOf oFormulario Is frmAbrirTurno Then
-                lAguardar = True
-              End If
-            Next
-            Application.DoEvents()
-          End While
-        End With
-      End If
-    End If
+        If Not rCaixa.Situacao Then
+            If MessageBox.Show("A abertura de turno ainda não foi realizada." & vbNewLine & "Deseja efetuar a abertura agora?", "Turno não iniciado", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = DialogResult.Yes Then
+                With frmAbrirTurno
+                    '.MdiParent = frmPrincipal
+                    .Show()
+                    lAguardar = True
+                    While lAguardar
+                        lAguardar = False
+                        For Each oFormulario In Application.OpenForms
+                            If TypeOf oFormulario Is frmAbrirTurno Then
+                                lAguardar = True
+                            End If
+                        Next
+                        Application.DoEvents()
+                    End While
+                End With
+            End If
+        End If
 
-        Return True 'rCaixa.Situacao
+        Return rCaixa.Situacao
 
     End Function
 
