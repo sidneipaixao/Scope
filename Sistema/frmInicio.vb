0e7ca59ea44ac0f062b457f93b4eedc9aa740dcb
sidneipaixao <34635384+sidneipaixao@users.noreply.github.com>
2018-01-24 06:02:12 +0000

    Busca pedido e integracao SAT
    Implementado codigo para busca automatica de pedidos e atualizacao da tela.
    Implementado codigo para envio dos dados para o equipamento SAT.

--- Sistema/frmInicio.vb
+++ Sistema/frmInicio.vb
@@ -3,7 +3,10 @@
     Private Sub frmInicio_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
         Application.DoEvents()
-        fnConectar()
+        If Not fnConectar() Then
+            MessageBox.Show("Erro ao efetuar a conexão na base de dados. A aplicação não poderá continuar", "Conexão não estabelecida", MessageBoxButtons.OK, MessageBoxIcon.Stop)
+            End
+        End If
 
     End Sub
 
--- Sistema/frmPedidos.vb
+++ Sistema/frmPedidos.vb
@@ -12,47 +12,7 @@
         BackgroundImage = New Bitmap(Width - 1, Height - 1)
         Graphics.FromImage(BackgroundImage).DrawRectangle(New Pen(Color.FromArgb(55, 65, 80)), New Rectangle(New Point(0, 0), Size))
 
-        'Dim oPedidos As SqlClient.SqlDataReader = fnRetornaDados("SELECT 1 CODPEDIDO, 'Luis de Camões' NOME, '...' CONTRATO, 'luis.camoes@art.com.br' EMAIL UNION " & _
-        '                                                         "SELECT 2 CODPEDIDO, 'Neil deGrasse Tyson' NOME, 'Renner' CONTRATO, '...' EMAIL UNION " & _
-        '                                                         "SELECT 3 CODPEDIDO, 'Phil Spencer' NOME, '...' CONTRATO, 'phil.spencer@microsoft.com' EMAIL UNION " & _
-        '                                                         "SELECT 3 CODPEDIDO, 'Verner Von Siemens' NOME, 'Fragrance' CONTRATO, '...' EMAIL UNION " & _
-        '                                                         "SELECT 4 CODPEDIDO, 'Charles Miller' NOME, 'Riachuelo' CONTRATO, 'cliente@riachuelo.com.br' EMAIL")
-        Dim oPedidos As SqlClient.SqlDataReader = fnRetornaDados("SELECT VNDCODIGO CODPEDIDO, VNDCLIENTENOME NOME, '' CONTRATO, VNDCLIENTEEMAIL EMAIL FROM vendas WHERE VNDCOMANDA IS NULL")
-
-        lstPedidos.Items.Clear()
-
-        'CARREGA PEDIDOS DA BASE DE DADOS
-        While oPedidos.Read
-            With lstPedidos.Items.Add("PDD" & oPedidos("CODPEDIDO"), oPedidos("NOME") & "", 0)
-                .UseItemStyleForSubItems = False
-                .SubItems.Add(oPedidos("CONTRATO") & " ", Color.IndianRed, lstPedidos.BackColor, New Font(lstPedidos.Font.Name, 12, FontStyle.Bold))
-                .SubItems.Add(oPedidos("EMAIL") & " ", Color.Gray, lstPedidos.BackColor, New Font(lstPedidos.Font.Name, 12, FontStyle.Bold))
-            End With
-        End While
-
-        oPedidos.Close()
-
-        'oPedidos = fnRetornaDados("SELECT 1 CODPEDIDO, 'Antonio Banderas' NOME, 'New México' CONTRATO, 'antonio.banderas@dmail.com' EMAIL UNION " & _
-        '                                                         "SELECT 2 CODPEDIDO, 'Alcebiades P. Vasconcelos' NOME, 'Renner' CONTRATO, 'alce@vascon.com.br' EMAIL UNION " & _
-        '                                                         "SELECT 3 CODPEDIDO, 'Alessandra Vieira Ramos' NOME, '...' CONTRATO, 'alessandraVR@dmail.com' EMAIL")
-        'oPedidos = fnRetornaDados("SELECT ID CODPEDIDO, IDENTIFICACAO NOME, '' CONTRATO, IF(FIDELIDADE='N', 'Cliente nao fidelizado', FIDELIDADE) EMAIL FROM vendas WHERE ID BETWEEN 492041 AND 492050")
-
-        'lstRecentes.Items.Clear()
-
-        ''CARREGA PEDIDOS DA BASE DE DADOS
-        'While oPedidos.Read
-        '    With lstRecentes.Items.Add("PDD" & oPedidos("CODPEDIDO"), oPedidos("NOME") & "", 0)
-        '        .UseItemStyleForSubItems = False
-        '        .SubItems.Add(oPedidos("CONTRATO") & " ", Color.IndianRed, lstPedidos.BackColor, New Font(lstPedidos.Font.Name, 12, FontStyle.Bold))
-        '        .SubItems.Add(oPedidos("EMAIL") & " ", Color.Gray, lstPedidos.BackColor, New Font(lstPedidos.Font.Name, 12, FontStyle.Bold))
-        '    End With
-        'End While
-
-        'oPedidos.Close()
-
-        'FECHA A CONEXAO COM O MYSQL, PORQUE ESTA VIA INTERNET
-        'oConMySQL.Close()
-
+        objBuscaPedido_Tick(objBuscaPedido, Nothing)
 
     End Sub
 
@@ -75,7 +35,28 @@
 
     End Sub
 
-    Private Sub lstRecentes_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles lstRecentes.SelectedIndexChanged
+    Private Sub objBuscaPedido_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles objBuscaPedido.Tick
+
+        ''EXECUTA A CHAMADA DA ROTINA NA BASE DE DADOS
+        'oComando.CommandText = "SP_BUSCAPEDIDO"
+        'oComando.CommandTimeout = 600
+        'oComando.ExecuteNonQuery()
+
+        Dim oPedidos As SqlClient.SqlDataReader = fnRetornaDados("SELECT VNDCODIGO CODPEDIDO, VNDCLIENTENOME NOME, '' CONTRATO, VNDCLIENTEEMAIL EMAIL FROM vendas WHERE VNDCOMANDA IS NULL")
+
+        lstPedidos.Items.Clear()
+
+        'CARREGA PEDIDOS DA BASE DE DADOS
+        While oPedidos.Read
+            With lstPedidos.Items.Add("PDD" & oPedidos("CODPEDIDO"), oPedidos("NOME") & "", 0)
+                .UseItemStyleForSubItems = False
+                .SubItems.Add(oPedidos("CONTRATO") & " ", Color.IndianRed, lstPedidos.BackColor, New Font(lstPedidos.Font.Name, 12, FontStyle.Bold))
+                .SubItems.Add(oPedidos("EMAIL") & " ", Color.Gray, lstPedidos.BackColor, New Font(lstPedidos.Font.Name, 12, FontStyle.Bold))
+            End With
+        End While
+
+        oPedidos.Close()
 
     End Sub
+
 End Class
